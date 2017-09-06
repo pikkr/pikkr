e941e026e1399b0865776ef1fe7da237834847d3
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2017-09-06 19:12:09 +0900

    apply rustfmt
    The version of formatter: rustfmt nightly-0.2.5

--- src/query.rs
+++ src/query.rs
@@ -5,6 +5,6 @@ pub struct Query<'a> {
     pub i: usize,
     pub ri: usize,
     pub target: bool,
-    pub children: Option<FnvHashMap<&'a[u8], Query<'a>>>,
+    pub children: Option<FnvHashMap<&'a [u8], Query<'a>>>,
     pub children_len: usize,
 }
