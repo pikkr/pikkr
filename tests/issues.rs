4ba0e3a8d44b2ebb07ef845ef37666b662c5e0ce
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2017-09-07 18:25:26 +0900

    add a test case

--- tests/issues.rs
+++ tests/issues.rs
@@ -5,6 +5,10 @@ mod issues {
     use pikkr::Pikkr;
     use pikkr::ErrorKind;
 
+    /// The helper macro for test cases.
+    ///
+    /// The first argument is the parameters used in `Pikkr::new` and `Pikkr::parse`.
+    /// The remaining tokens after `=>` are used as the pattern to match success case.
     macro_rules! do_parse {
         ($p:expr => $($ptn:tt)*) => {{
             let res = Pikkr::new($p.0, $p.1).map(|mut p| p.parse($p.2));
@@ -22,6 +26,13 @@ mod issues {
         let t = 1;
         let r = "";
         do_parse!((q, t, r) => Ok(Err(ref e)) if e.kind() == ErrorKind::InvalidRecord);
+    }
+
+    #[test]
+    fn issue_15_panic_in_query_parser() {
+        let q = &["$"];
+        let t = 1;
+        do_parse!((q, t, "") => Err(ref e) if e.kind() == ErrorKind::InvalidQuery);
     }
 
     // test cases for unclosed issues.
