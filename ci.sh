9e63779207c9e6134732a7e54dc0a49ac4275a31
Keiji Yoshida <kjmrknsn@gmail.com>
2017-09-09 19:59:19 +0900

    Make the order of rustfmt last in CI

--- ci.sh
+++ ci.sh
@@ -2,11 +2,11 @@
 
 export RUSTFLAGS="-C target-cpu=native"
 
-rustfmt_installed=$(cargo install --list | grep rustfmt-nightly | wc -l)
+cargo build --release
+cargo test --release
 
+rustfmt_installed=$(cargo install --list | grep rustfmt-nightly | wc -l)
 if [ $rustfmt_installed -eq 0 ]; then
     cargo install rustfmt-nightly
 fi
 cargo fmt -- --write-mode=diff
-cargo build --release
-cargo test --release
