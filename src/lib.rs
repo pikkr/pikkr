1ab7258baf448fe9e05512926d8c2eba2e55060c
Andre Bogus <bogusandre@gmail.com>
2017-09-11 22:14:39 +0200

    allow build without AVX

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 //! JSON parser which picks up values directly without performing tokenization
 extern crate fnv;
+#[cfg(feature = "avx-accel")]
 extern crate x86intrin;
 
+#[cfg(feature = "avx-accel")]
 mod avx;
+#[cfg(not(feature = "avx-accel"))]
+mod emulated;
 mod bit;
 mod error;
 mod index_builder;
@@ -11,6 +15,9 @@ mod pikkr;
 mod query;
 mod result;
 mod utf8;
+
+#[cfg(not(feature = "avx-accel"))]
+pub use emulated::avx;
 
 pub use error::{Error, ErrorKind};
 pub use pikkr::Pikkr;
