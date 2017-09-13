ed79175f528d76cbf6fb4a5830c87f7c04dcf10a
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2017-09-13 18:13:22 +0900

    chore(ci): fix .travis.yml

--- .travis/format_diff.sh
+++ .travis/format_diff.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -eu
 
-export RUSTFLAGS="-C target-cpu=native"
-
-cargo build --release
-cargo test --release
-cargo bench
-
 rustfmt_installed=$(cargo install --list | grep rustfmt-nightly | wc -l)
 if [ $rustfmt_installed -eq 0 ]; then
     cargo install rustfmt-nightly
