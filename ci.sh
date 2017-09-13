e48038f1358445690a1d90fae6682a4155e8b7ae
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2017-09-13 18:11:41 +0900

    chore(ci): run benchmark tests on CI

--- ci.sh
+++ ci.sh
@@ -4,6 +4,7 @@ export RUSTFLAGS="-C target-cpu=native"
 
 cargo build --release
 cargo test --release
+cargo bench
 
 rustfmt_installed=$(cargo install --list | grep rustfmt-nightly | wc -l)
 if [ $rustfmt_installed -eq 0 ]; then
